04ed44eb14fce9bd49a1f16c32b46cf7c8e7b77c
sbkubric <sbkubric@gmail.com>
2023-04-26 01:22:18 +0300

    feature: cache dummy

--- src/services/cache.py
+++ src/services/cache.py
@@ -1,8 +1,11 @@
 from datetime import datetime, timedelta
 from typing import Any, Callable
+from urllib.parse import quote_plus
 
-from core.config import Settings
-from db. import Cache
+from fastapi import Request
+
+from core.config import settings
+from db.redis import Cache
 
 
 def expired(cached: dict[str, Any]) -> bool:
@@ -11,20 +14,25 @@ def expired(cached: dict[str, Any]) -> bool:
     if not timestamp:
         raise Exception
 
-    delta = timedelta(seconds=Settings.API_CACHE_EXPIRES_IN_SECONDS)
+    delta = timedelta(seconds=settings.cache_expiration_in_seconds)
     if datetime.now() - timestamp >= delta:
         return True
 
     return False
 
 
-def prepare_url_query(*args, **kwargs) -> str:
-    return ""
+def prepare_url_query(request: Request) -> str:
+    url = request.url.path + "?"
+    for param, value in request.query_params.items():
+        if not url.endswith("?"):
+            url = url + "&"
+        url = f"{url}{param}={quote_plus(value)}"
+    return url
 
 
 def cache(func) -> Callable:
-    async def inner(*args, **kwargs):
-        url_query = prepare_url_query(*args, **kwargs)
+    async def inner(*args, request: Request, **kwargs):
+        url_query = prepare_url_query(request)
         cached_response = await Cache.get(url_query)
         response = cached_response.get('response')
         if expired(cached_response):
