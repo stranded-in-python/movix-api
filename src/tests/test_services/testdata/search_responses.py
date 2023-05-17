9c374722a166e19ba6d58e3da231d3b326e49d86
Sergey <sergeusprecious@gmail.com>
2023-05-17 18:53:34 +0300

    fix/tests person test_search_persons_ok and test_search_person_ok

--- src/tests/test_services/testdata/search_responses.py
+++ src/tests/test_services/testdata/search_responses.py
@@ -14,33 +14,29 @@ SEARCH_FILMS_SUCCESS = [
 SEARCH_FILMS_NOT_FOUND = {"detail": "film not found"}
 
 SEARCH_PERSONS_SUCCESS = [
-  {
-    "uuid": "6ba834a0-1039-4b5c-ab78-d5f1f18a4e7d",
-    "full_name": "Carrie Beck",
-    "films": [
-      {
-        "uuid": "5065b37b-fd5b-4c48-8a24-435198c44830",
-        "title": "Star Wars Resistance",
-        "imdb_rating": 4.9,
-        "roles": [
-          "writer"
-        ]
-      },
-      {
-        "uuid": "754589c1-e304-4ed8-87bc-1ac897529b97",
-        "title": "Lego Star Wars: The Freemaker Adventures",
-        "imdb_rating": 7.5,
-        "roles": [
-          "writer"
-        ]
-      }
-    ]
-  },
-  {
-    "uuid": "cbceaa01-910c-4f0e-b992-7faf7d27131a",
-    "full_name": "Carrie Underwood",
-    "films": []
-  }
+    {
+        "uuid": "6ba834a0-1039-4b5c-ab78-d5f1f18a4e7d",
+        "full_name": "Carrie Beck",
+        "films": [
+            {
+                "uuid": "5065b37b-fd5b-4c48-8a24-435198c44830",
+                "title": "Star Wars Resistance",
+                "imdb_rating": 4.9,
+                "roles": ["writer"],
+            },
+            {
+                "uuid": "754589c1-e304-4ed8-87bc-1ac897529b97",
+                "title": "Lego Star Wars: The Freemaker Adventures",
+                "imdb_rating": 7.5,
+                "roles": ["writer"],
+            },
+        ],
+    },
+    {
+        "uuid": "cbceaa01-910c-4f0e-b992-7faf7d27131a",
+        "full_name": "Carrie Underwood",
+        "films": [],
+    },
 ]
 
 SEARCH_PERSONS_NOT_FOUND = {"detail": "persons not found"}
